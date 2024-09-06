18a30631d8a11da66f6aea087288e2549f4897eb
Claudio Silva <156344616+Claudiosomm@users.noreply.github.com>
2024-09-06 10:55:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+{
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, conhecida como Fadinha, é uma skatista profissional brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando o bronze nos Jogos Olímpicos de Tóquio.",
+    link : "https://www.google.com/search?q=Rayssa+leal&sca_esv=abed81e574b41454&sxsrf=ADLYWIJpT6ShBmkayG_jRxECkefskCuvig%3A1725545251702&ei=I7vZZu6jKsfP1sQPhO6BaA&ved=0ahUKEwiuwZX-_KuIAxXHp5UCHQR3AA0Q4dUDCBA&uact=5&oq=Rayssa+leal&gs_lp=Egxnd3Mtd2l6LXNlcnAiC1JheXNzYSBsZWFsMggQLhiABBixAzIKEAAYgAQYQxiKBTIFEAAYgAQyChAAGIAEGEMYigUyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyFxAuGIAEGLEDGJcFGNwEGN4EGN8E2AEDSKwcUL4EWPAUcAF4AZABAJgBiQGgAeIEqgEDMC41uAEDyAEA-AEBmAIGoALWBcICChAAGLADGNYEGEfCAg0QABiABBiwAxhDGIoFwgIOEAAYsAMY5AIY1gTYAQHCAhMQLhiABBiwAxhDGMgDGIoF2AECwgIQEC4YgAQYsQMYQxiDARiKBZgDAIgGAZAGE7oGBggBEAEYCboGBggCEAEYCLoGBggDEAEYFJIHAzEuNaAHj0I&sclient=gws-wiz-serp",
+    tags: "skate fada sk8"
+    
+},
+{
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Andrade é uma ginasta artística brasileira que fez história ao se tornar a maior medalhista olímpica do país. Com uma combinação impressionante de força, agilidade e elegância, Rebeca conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+    link : "https://www.google.com/search?q=rebeca+andrade&sca_esv=cdb79dfeeefb1ef1&sxsrf=ADLYWIJenYn0s4ED2vJOc1k-3Wp6Efj1sQ%3A1725382986793&ei=SkHXZoWLMP_51sQPjo6d4Aw",
+    tags: "ginastica ginástica ginasta ouro"
+},
+{
+    titulo : "Beatriz Souza",
+    descricao : "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024. Sua técnica impecável e determinação a levaram ao topo do pódio, inspirando uma nova geração de judocas.",
+    link : "https://www.google.com/search?q=beatriz+souza&oq=beatriz+souza&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyDwgBEC4YQxixAxiABBiKBTIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDIHCAcQABiABDIHCAgQABiABDIHCAkQABiABNIBCTc0NDNqMGoxNagCALACAA&sourceid=chrome&ie=UTF-8",
+    tags: "judô judo judoca judoka beatris Beatris"
+}
+];
+
